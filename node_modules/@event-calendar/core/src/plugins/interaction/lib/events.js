b6f95b0c4f6a43d4c3c8d6da1b96d0a07b0970ff
LEESANGHWA <shlee@kuls.co.kr>
2025-08-04 14:07:19 +0900

    gitignore

--- node_modules/@event-calendar/core/src/plugins/interaction/lib/events.js
+++
@@ -1,9 +0,0 @@
-import {bgEvent, helperEvent} from '#lib';
-
-export function eventDraggable(event, $eventStartEditable, $editable) {
-    return (event.startEditable ?? $eventStartEditable ?? event.editable ?? $editable);
-}
-
-export function eventResizable(event, $eventDurationEditable, $editable) {
-    return (event.durationEditable ?? $eventDurationEditable ?? event.editable ?? $editable);
-}
